ac36ff795c24c6c50896b7e44911352acf8e60f8
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2021-07-08 14:25:29 +0300

    add

--- app.js
+++ app.js
@@ -53,17 +53,37 @@ function createPreview(recipe){
         e('div', { className: 'small' }, e('img', { src: recipe.img }))
     )
 
-    result.addEventListener('click', () => getRecipeDetails(recipe._id));
+    result.addEventListener('click', () => getRecipeDetails(recipe._id, result));
 
     return result;
 }
 
-async function getRecipeDetails(id){
+async function getRecipeDetails(id, preview){
     const url = 'http://localhost:3030/jsonstore/cookbook/details/' + id;
 
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    
+    const result = e('article', {},
+        e('h2', { onClick: toggleCard }, data.name),
+        e('div', { className: 'band' },
+            e('div', { className: 'thumb' }, e('img', { src: data.img })),
+            e('div', { className: 'ingredients' },
+                e('h3', {}, 'Ingredients:'),
+                e('ul', {}, data.ingredients.map(i => e('li', {}, i)))
+            )
+        ),
+        e('div', { className: 'description' },
+            e('h3', {}, 'Preparation:'),
+            data.steps.map(s => e('p', {}, s))
+        )
+    );
+
+    preview.replaceWith(result);
+
+    function toggleCard(){
+        result.replaceWith(preview);
+    }
 }
 
 window.addEventListener("load", () => {
